03692686cf67abdf3986bd805ea9d32d217e1f2e
lokesh525 <lokesh.chandra525@gmail.com>
2018-10-25 01:11:21 +0530

    Lost and found phase complete

---
+++ src/myproject/MSQLConn2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lokesh Chandra
+ */
+public class MSQLConn2 {
+        
+       public static void main(String args[])
+       {
+           new MSQLConn2();
+       }
+    
+}
---
+++ src/myproject/MSQLConnection.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lokesh Chandra
+ */
+public class MSQLConnection {
+        
+       public static void main(String args[])
+       {
+           new MSQLConnection();
+       }
+}
